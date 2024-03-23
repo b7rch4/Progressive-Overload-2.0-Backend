02a8bbfc30a4a4a61074b7cc14ab6c783a8c7045
b7rch4 <james.birchall4@btinternet.com>
2024-03-23 09:12:05 +0000

    Renaming exercise app to api.
    Previously there was a specific app for exercises, with a plan to separate all different areas of the backend.
    Now combining in one api app.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Exercises
+from .models import Exercise
 
 # Create your views here.
 def exercises(request):
-	exercises = Exercises.objects.all()
+	exercises = Exercise.objects.all()
 	return JsonResponse(list(exercises.values()), safe=False)
