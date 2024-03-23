7eb3c1f502452fcc6aa40fd03dd79c7ae42f1895
b7rch4 <james.birchall4@btinternet.com>
2024-03-23 11:56:54 +0000

    Modelling workouts, exercises and sets.
    Added models to handle workouts and exercises and sets.

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Exercise
+from .models import Exercise, Workout, Set, WorkoutExercise
 
 # Register your models here.
 admin.site.register(
 	Exercise
+)
+admin.site.register(
+	Workout
+)
+admin.site.register(
+	Set
+)
+admin.site.register(
+	WorkoutExercise
 )
--- api/models.py
+++ api/models.py
@@ -3,13 +3,40 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Exercise(models.Model):
-        exercise_name = models.CharField(max_length=250)
-        muscle_group = models.CharField(max_length=250)
-        equipment = models.CharField(max_length=250)
+        name = models.CharField(max_length=100)
+        muscle_group = models.CharField(max_length=50)
         created_date = models.DateTimeField(default=timezone.now)
 
         def __str__(self):
-            return self.exercise_name
+            return self.name
+        
+class Workout(models.Model):
+      user = models.ForeignKey(User, on_delete=models.CASCADE)
+      name = models.CharField(max_length=100)
+
+      def __str__(self):
+            return self.name
+
+class Set(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    reps = models.IntegerField()
+    weight = models.FloatField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.exercise.name)
+    
+class WorkoutExercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.ManyToManyField(Set)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.workout.name} - {self.exercise.name}"
